2f4f69a1d15af0a95748b42b67fb47277b187a97
oolorundara <54598746+oolorundara@users.noreply.github.com>
2019-11-28 10:04:04 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿function init(){
+//add your javascript between these two lines of code
+
+var a = document.getElementsByTagName("h2")[0].innerHTML;
+var b = document.getElementById("entryinput").value;
+
+alert(a + ": " + b);
+document.getElementById("textoutput").innerHTML = b;
+}
+
+window.addEventListener('load', init);
